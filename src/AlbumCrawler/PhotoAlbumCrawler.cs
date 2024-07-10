3f67549f63d9cfb93806820a3e8bd97693d57f03
Paul <paul@goodhops.com>
2024-07-09 21:23:34 -0400

    Refactor and make albums immutable while mergineg configuration.

--- src/AlbumCrawler/PhotoAlbumCrawler.cs
+++ src/AlbumCrawler/PhotoAlbumCrawler.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.Options;
 
 namespace AlbumCrawler
 {
+    /// <summary>
+    /// crawls the file system for photo albums.  The albums are created from folders
+    /// containing files with the specified extensions.
+    /// </summary>
     public class PhotoAlbumCrawler
     {
         private readonly AlbumCrawlerOptions _albumCrawlerOptions;
@@ -16,6 +20,11 @@ namespace AlbumCrawler
             _albumCrawlerOptions = options.Value;
             _webHostEnvironment = webHostEnvironment;
         }
+        /// <summary>
+        /// Use FolderCrawler to find all folders containing photos and create an Album
+        /// for each.  If there are any overrides in the AlbumCrawlerOptions, apply them.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Album> Crawl()
         {
             string startingFolderFullPath = ".";
@@ -38,40 +47,61 @@ namespace AlbumCrawler
 
             return albums;
         }
+        /// <summary>
+        /// Create a default Album from the folder.  The default Album uses the Name of the 
+        /// folder for both the name and path.  The thumbnail is the first file in the folder.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
         public Album CreateDefaultAlbumFromFolder(Folder folder)
         {
             string thumbnail = folder.Files.Count > 0 ? folder.Files[0].Name : "";
 
             return new Album(folder.Name, folder.Name, "", thumbnail);
         }
+        /// <summary>
+        /// Create an Album from the folder.  If there are any overrides in the AlbumCrawlerOptions,
+        /// use them to provide more meaningful values for the Album.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
         public Album CreateAlbumFromFolder(Folder folder)
         {
             Album defaultAlbum = CreateDefaultAlbumFromFolder(folder);
 
             return MergeWithOverrides(defaultAlbum);
         }
+        /// <summary>
+        /// Given the album automatically created from the folder, merge any overrides
+        /// specified in the AlbumCrawlerOptions.
+        /// </summary>
+        /// <param name="defaultAlbum"></param>
+        /// <returns></returns>
         public Album MergeWithOverrides(Album defaultAlbum)
         {
+            Album album = defaultAlbum;
             if (_albumCrawlerOptions.Albums != null && _albumCrawlerOptions.Albums.Length > 0)
             {
+                album = new Album(defaultAlbum);
+
                 AlbumOptions? albumOptions = _albumCrawlerOptions.Albums.FirstOrDefault(a => a.Path == defaultAlbum.Path);
                 if (albumOptions != null)
                 {
                     if (!String.IsNullOrEmpty(albumOptions.Name))
                     {
-                        defaultAlbum.Name = albumOptions.Name;
+                        album.Name = albumOptions.Name;
                     }
                     if (!String.IsNullOrEmpty(albumOptions.Description))
                     {
-                        defaultAlbum.Description = albumOptions.Description;
+                        album.Description = albumOptions.Description;
                     }
                     if (!String.IsNullOrEmpty(albumOptions.Thumbnail))
                     {
-                        defaultAlbum.Thumbnail = albumOptions.Thumbnail;
+                        album.Thumbnail = albumOptions.Thumbnail;
                     }
                 }
             }
-            return defaultAlbum;
+            return album;
         }
         /// <summary>
         /// create the wildcard patterns to search for files with the specified extensions.
@@ -91,6 +121,11 @@ namespace AlbumCrawler
 
             return patterns;
         }
+        /// <summary>
+        /// maps the path relative to the web root to a full path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         protected string MapPath(string path)
         {
             if (path.EndsWith("/") || path.EndsWith("\\"))
